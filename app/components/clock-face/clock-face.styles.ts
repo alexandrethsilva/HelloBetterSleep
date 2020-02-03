d3de6ae0eea4ab6dc7c2599d5e2f1aa5e6bce212
Alexandre Theodoro da Silva <alexandrethsilva@mac.com>
2020-02-03 12:26:45 +0100

    refactor(clock-slider, clock-face): remove dangling components from previous version

--- app/components/clock-face/clock-face.styles.ts
+++
@@ -1,7 +0,0 @@
-import { ViewStyle } from "react-native"
-
-export const clockFaceStyles = {
-  WRAPPER: {
-    justifyContent: 'center'
-  } as ViewStyle
-}
--- app/components/clock-slider/clock-slider.styles.ts
+++
@@ -1,7 +0,0 @@
-import { ViewStyle } from "react-native"
-
-export const circularSliderStyles = {
-  WRAPPER: {
-    justifyContent: 'center'
-  } as ViewStyle
-}
